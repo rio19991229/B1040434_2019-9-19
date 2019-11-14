a9461daacec0b147402fe0fac8637365bb21c809
rio19991229 <55218512+rio19991229@users.noreply.github.com>
2019-11-14 10:14:13 +0800

    2019/11/14

--- MotionCapture/Assets/Script/Lady.cs
+++ MotionCapture/Assets/Script/Lady.cs
@@ -2,6 +2,39 @@
 
 public class Lady : MonoBehaviour
 {
+
+    private Animator ani;
+    private Rigidbody rig;
+
+
+    [Header("速度"), Range(0f, 50f)]
+    public float speed = 10f;
+
+
+    [Header("動畫控制器")]
+
+    public string aniWalk = "走路開關";
+    public string aniHurt = "受傷觸發";
+    public string aniAtk = "攻擊01觸發";
+    public string aniJump = "跳躍觸發";
+    public string aniDead = "死亡開關";
+
+
+
+
+    private void Start()
+    {
+        ani = GetComponent<Animator>();
+        rig = GetComponent<Rigidbody>();
+    }
+
+
+    private void Update()
+    {
+        Walk();
+        Jump();
+        Attack();
+    }
     // 定義方法
     // 修飾詞 傳回類型 方法名稱 ( 參數 ) { 敘述 }
     // void 無回傳
@@ -12,7 +45,9 @@ public class Lady : MonoBehaviour
     /// </summary>
     private void Walk()
     {
-
+        ani.SetBool(aniWalk, Input.GetAxis("Vertical") != 0|| Input.GetAxis("Horizontal") != 0);
+        //rig.AddForce(0, 0, Input.GetAxisRaw("Vertical") * speed);             // 以世界座標移動
+        rig.AddForce(transform.forward * Input.GetAxisRaw("Vertical") * speed); // 以區域座標移動
     }    
    
     /// <summary>
@@ -20,6 +55,10 @@ public class Lady : MonoBehaviour
     /// </summary>
     private void Attack()
     {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            ani.SetTrigger(aniAtk);
+        }
 
     }    
     
@@ -28,22 +67,30 @@ public class Lady : MonoBehaviour
     /// </summary>
     private void Jump()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ani.SetTrigger(aniJump);
+        }
 
-    }    
-    
+    }
+
     /// <summary>
     /// 受傷
     /// </summary>
     private void Hurt()
     {
 
-    }    
-    
+      ani.SetTrigger(aniHurt);
+
+    }
+
     /// <summary>
     /// 死亡
     /// </summary>
     private void Dead()
     {
+
+        ani.SetTrigger(aniDead);
 
     }
 
